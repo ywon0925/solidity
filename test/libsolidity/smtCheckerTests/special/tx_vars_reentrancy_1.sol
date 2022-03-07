7152c54e219fae7c87a8365715c6f01d827fc23b
Leo Alt <leo@ethereum.org>
2022-03-07 13:28:13 +0100

    adjust old tests

--- test/libsolidity/smtCheckerTests/special/tx_vars_reentrancy_1.sol
+++ test/libsolidity/smtCheckerTests/special/tx_vars_reentrancy_1.sol
@@ -11,6 +11,7 @@ contract C {
 }
 // ====
 // SMTEngine: all
+// SMTIgnoreCex: yes
 // SMTIgnoreOS: macos
 // ----
-// Warning 6328: (135-169): CHC: Assertion violation happens here.\nCounterexample:\n\n_i = 0\nx = 2997\n\nTransaction trace:\nC.constructor()\nC.g(0){ msg.value: 2803 }\n    _i.f() -- untrusted external call, synthesized as:\n        C.g(0){ msg.value: 2446 } -- reentrant call\n            _i.f() -- untrusted external call
+// Warning 6328: (135-169): CHC: Assertion violation happens here.
