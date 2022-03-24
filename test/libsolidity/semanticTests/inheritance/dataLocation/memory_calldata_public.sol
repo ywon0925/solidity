13b0fb8ae15b3adbcb4b4aafc67b41aeccb8ccc0
chriseth <chris@ethereum.org>
2022-03-24 15:03:02 +0100

    Tests for changing data location in inheritance.

---
+++ test/libsolidity/semanticTests/inheritance/dataLocation/memory_calldata_public.sol
@@ -0,0 +1,18 @@
+abstract contract A {
+    function f(uint256[1] memory a) public virtual returns (uint256);
+    function test() external returns (uint) {
+        uint[1] memory t;
+        t[0] = 7;
+        return f(t);
+    }
+}
+
+contract B is A {
+    function f(uint256[1] calldata a) public override returns (uint256) {
+        return a[0];
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// test() -> 0
